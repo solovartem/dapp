4ce2d22528a75df2a7b8a289c50f1607f0cc1edd
Artem <artemsolovev1201@gmail.com>
2020-01-11 11:40:53 +0400

    modifier

--- MyContract.sol
+++ MyContract.sol
@@ -1,40 +1,31 @@
 pragma solidity ^0.5.1;
 
 contract MyContract {
+    mapping(uint8 => Person) public people;
+    uint8 public size = 0;
+    address owner;
     
- enum Operations { Sum, Minus}
- Operations internal operations;
- 
- function activeSum() public {
-     operations = Operations.Sum;
- }
- 
- function activeMinus() public {
-     operations = Operations.Minus;
- }
- 
- function calculate(int a, int b) public view returns(int) {
-     if (operations == Operations.Sum) 
-        return a + b;    
-     return a - b; 
- }
- 
- struct Person {
-    string _firstname;
-    string _lastname;
-    int8 age;
- }
- 
- Person[] public persons;
- 
- function addPerson(string memory name, string memory surname, int8 age) public {
-     persons.push(Person(name, surname, age));
- }
- 
- mapping(bytes32 => string) public people;
- 
- function setExample(bytes32 key, string memory value) public {
-     people[key] = value;
- }
- 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    struct Person {
+        string _name;
+        string _surname;
+    }
+    
+    function addPerson(string memory _name, string memory _surname) public onlyOwner {
+        incrementSize();
+        people[size] = Person(_name, _surname);
+    }
+    
+    function incrementSize() internal {
+        size ++;
+    }
+    
 }
