6288e73b6982df18129bc8c3a1632a12bb9867b4
Artem <artemsolovev1201@gmail.com>
2020-01-12 11:25:44 +0400

    enh

--- FirstsmartContract.sol
+++ FirstsmartContract.sol
@@ -5,24 +5,24 @@ contract ERC20Token {
     string name;
     mapping (address => uint256) public balances;
     
+    constructor(string memory _name) public {
+        name = _name;
+    }
+    
     function mint() public {
         balances[tx.origin]++;
     }
 }
 
-contract FirstSmartContract {
-    
-    address payable private wallet;
-    address public token;
-    
-    constructor(address payable _wallet, address _tokenAddress) public {
-        wallet = _wallet;
-        token = _tokenAddress;
+contract MyToken is ERC20Token {
+    string public symbol;
+    address[] public owners;
+    constructor(string memory _name, string memory _symbol) ERC20Token(_name) public {
+        symbol = _symbol;
     }
     
-    function buyToken() public payable {
-        ERC20Token _token = ERC20Token(token);
-        _token.mint();
-        wallet.transfer(msg.value);
+    function mint() public {
+        super.mint();
+        owners.push(msg.sender);
     }
 }
