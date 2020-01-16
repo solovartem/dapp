da0196b28055f302a6a12a35217f2eba45c5734c
Artem <artemsolovev1201@gmail.com>
2020-01-16 21:55:33 +0400

    prett file

--- web3/main.js
+++ web3/main.js
@@ -3,13 +3,8 @@
   const url = "https://mainnet.infura.io/v3/d1d21a61b32a4f669b1438e9218cc279"
   const web3 = new Web3(url)
   const address = "0x2C86E951da65ea73c8FfC7077a34614d3a115C2e"
-
-  async function findEtherByAccount(address, web3) {
-    if (typeof address !== "string") throw "Error"
-    const balance = await web3.eth.getBalance(address)
-    return balance
-  }
-
+  const findEtherByAccount = async (address, web3) => await web3.eth.getBalance(address)
   const weiBalance = await findEtherByAccount(address, web3)
   const ehterBalance = await web3.utils.fromWei(weiBalance, "ether")
+  const account = web3.eth.accounts.create()
 })()
